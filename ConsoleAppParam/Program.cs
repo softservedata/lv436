4dd634075fbfc098853dcd349efc1a09ade875eb
bebebe <hahaha@gmail.com>
2019-09-10 13:14:11 +0300

    Send Parameters C#. Examples

--- ConsoleAppParam/Program.cs
+++ ConsoleAppParam/Program.cs
@@ -6,10 +6,103 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppParam
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public void work1(int i)
         {
+            i = i + 1;
+            Console.WriteLine("work1(int i), i = " + i);
+        }
+
+        public void work2(ref int i)
+        {
+            i = i + 1;
+            Console.WriteLine("work2(ref int i), i = " + i);
+        }
+
+        public void work1(StringBuilder sb) // Overload
+        {
+            sb.Append(" 1234");
+            Console.WriteLine("work1(StringBuilder sb)), sb = " + sb);
+            sb = new StringBuilder("Ha-Ha-Ha");
+            Console.WriteLine("work1(StringBuilder sb)), new sb = " + sb);
+        }
+
+        public void work2(ref StringBuilder sb)
+        {
+            sb.Append(" 1234");
+            Console.WriteLine("work2(ref StringBuilder sb), sb = " + sb);
+            sb = new StringBuilder("Ha-Ha-Ha");
+            Console.WriteLine("work2(ref StringBuilder sb), new sb = " + sb);
+        }
+
+        public void work1(string s) // Overload
+        {
+            s = s + " 123";
+            Console.WriteLine("work1(string s), s = " + s);
+        }
+
+        public void work2(ref string s)
+        {
+            s = s + " 123";
+            Console.WriteLine("work2(ref string s), s = " + s);
+        }
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        public static void Main(string[] args)
+        {
+            Program appl = new Program();
+            //
+            // By Value/ Primitive(value) type
+            //int i = 1;
+            //Console.WriteLine("main(), start i = " + i);
+            //appl.work1(i);
+            //Console.WriteLine("main(), after i = " + i);
+            //
+            // By Reference/Primitive(value) type
+            //int i = 1;
+            //Console.WriteLine("main(), start i = " + i);
+            //appl.work2(ref i);
+            //Console.WriteLine("main(), after i = " + i);
+            //
+            // By Value/Reference type
+            //StringBuilder sb = new StringBuilder("abc");
+            //Console.WriteLine("main(), start sb = " + sb);
+            //StringBuilder sb0 = sb;
+            //Console.WriteLine("main(), start sb0 = " + sb0);
+            //appl.work1(sb);
+            //Console.WriteLine("main(), after sb = " + sb);
+            //Console.WriteLine("main(), start sb0 = " + sb0);
+            //
+            // By Reference/Reference type
+            //StringBuilder sb = new StringBuilder("abc");
+            //Console.WriteLine("main(), start sb = " + sb);
+            //StringBuilder sb0 = sb;
+            //Console.WriteLine("main(), start sb0 = " + sb0);
+            //appl.work2(ref sb);
+            //Console.WriteLine("main(), after sb = " + sb);
+            //Console.WriteLine("main(), start sb0 = " + sb0);
+            //
+            // By Value/Reference + Immutable type
+            //String s = new String("abc");
+            //string s = "abc";
+            //Console.WriteLine("main(), start s = " + s);
+            //string s0 = s;
+            //Console.WriteLine("main(), start s0 = " + s0);
+            //appl.work1(s);
+            //Console.WriteLine("main(), after s = " + s);
+            //Console.WriteLine("main(), after s0 = " + s0);
+            //
+            // By Reference/Reference + Immutable type
+            //String s = new String("abc");
+            //String s = "abc";
+            //Console.WriteLine("main(), start s = " + s);
+            //String s0 = s;
+            //Console.WriteLine("main(), start s0 = " + s0);
+            //appl.work2(ref s);
+            //Console.WriteLine("main(), after s = " + s);
+            //Console.WriteLine("main(), after s0 = " + s0);
+            //
         }
     }
 }
